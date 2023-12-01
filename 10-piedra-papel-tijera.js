2fe48a4219f02aa1a01cdfe6ba5152cde5758083
valrotkier <valrotkier@gmail.com>
2023-12-01 18:25:16 -0300

    probando un fix con toLowerCase()

--- 10-piedra-papel-tijera.js
+++ 10-piedra-papel-tijera.js
@@ -1,12 +1,13 @@
 function updateScoreElement() {
-    document.querySelector('.js-score')
-        .innerHTML = `Ganadas: ${puntaje.ganadas}, Perdidas: ${puntaje.perdidas}, Empates: ${puntaje.empates}`;
+  document.querySelector(
+    ".js-score"
+  ).innerHTML = `Ganadas: ${puntaje.ganadas}, Perdidas: ${puntaje.perdidas}, Empates: ${puntaje.empates}`;
 }
 //nuevo
-let puntaje = JSON.parse(localStorage.getItem('puntaje')) || {
-    ganadas: 0,
-    perdidas: 0,
-    empates: 0,
+let puntaje = JSON.parse(localStorage.getItem("puntaje")) || {
+  ganadas: 0,
+  perdidas: 0,
+  empates: 0,
 };
 /* Este codigo es lo mismo que arriba dsp del ||
 if (!puntaje) {
@@ -19,87 +20,71 @@ updateScoreElement();
 
 //fnuevo
 function pickComputerMove() {
-    const randomNumber = Math.random();
+  const randomNumber = Math.random();
 
-    let computerMove = '';
+  let computerMove = "";
 
-    if (randomNumber >= 0 && randomNumber < 1 / 3) {
-        computerMove = 'Piedra';
-    }
-    else if (randomNumber >= 1 / 3 && randomNumber < 2 / 3) {
-        computerMove = 'Papel';
-    }
-    else if (randomNumber >= 2 / 3 && randomNumber < 1) {
-        computerMove = 'Tijera';
-    }
+  if (randomNumber >= 0 && randomNumber < 1 / 3) {
+    computerMove = "Piedra";
+  } else if (randomNumber >= 1 / 3 && randomNumber < 2 / 3) {
+    computerMove = "Papel";
+  } else if (randomNumber >= 2 / 3 && randomNumber < 1) {
+    computerMove = "Tijera";
+  }
 
-    return computerMove;
+  return computerMove;
 }
 
 function playGame(playerMove) {
-    const computerMove = pickComputerMove();
+  const computerMove = pickComputerMove();
 
-    let resultado = '';
-    if (playerMove === 'Tijera') {
-        if (computerMove === 'Piedra') {
-            resultado = 'PERDISTE';
-        }
-        else if (computerMove === 'Papel') {
-            resultado = 'GANASTE';
-        }
-        else if (computerMove === 'Tijera') {
-            resultado = 'EMPATE';
-        }
-
-    } else if (playerMove === 'Papel') {
-        if (computerMove === 'Piedra') {
-            resultado = 'GANASTE';
-        }
-        else if (computerMove === 'Papel') {
-            resultado = 'EMPATE';
-
-        }
-        else if (computerMove === 'Tijera') {
-            resultado = 'PERDISTE';
-
-        }
-
-    } else if (playerMove === 'Piedra') {
-        if (computerMove === 'Piedra') {
-            resultado = 'EMPATE';
-        }
-        else if (computerMove === 'Papel') {
-            resultado = 'PERDISTE';
-
-        }
-        else if (computerMove === 'Tijera') {
-            resultado = 'GANASTE';
-
-        }
+  let resultado = "";
+  if (playerMove === "Tijera") {
+    if (computerMove === "Piedra") {
+      resultado = "PERDISTE";
+    } else if (computerMove === "Papel") {
+      resultado = "GANASTE";
+    } else if (computerMove === "Tijera") {
+      resultado = "EMPATE";
     }
-
-    //nuevo
-    if (resultado === 'GANASTE') {
-        puntaje.ganadas += 1;
-    } else if (resultado === 'PERDISTE') {
-        puntaje.perdidas += 1;
-    } else {
-        puntaje.empates += 1;
+  } else if (playerMove === "Papel") {
+    if (computerMove === "Piedra") {
+      resultado = "GANASTE";
+    } else if (computerMove === "Papel") {
+      resultado = "EMPATE";
+    } else if (computerMove === "Tijera") {
+      resultado = "PERDISTE";
     }
-    localStorage.setItem('puntaje', JSON.stringify(puntaje));
+  } else if (playerMove === "Piedra") {
+    if (computerMove === "Piedra") {
+      resultado = "EMPATE";
+    } else if (computerMove === "Papel") {
+      resultado = "PERDISTE";
+    } else if (computerMove === "Tijera") {
+      resultado = "GANASTE";
+    }
+  }
 
-    updateScoreElement();
-    //nuevo
+  //nuevo
+  if (resultado === "GANASTE") {
+    puntaje.ganadas += 1;
+  } else if (resultado === "PERDISTE") {
+    puntaje.perdidas += 1;
+  } else {
+    puntaje.empates += 1;
+  }
+  localStorage.setItem("puntaje", JSON.stringify(puntaje));
 
-    //agarro el paragraph y muestro el resultado
-    document.querySelector('.js-result').innerHTML = resultado;
+  updateScoreElement();
+  //nuevo
 
-    document.querySelector('.js-moves').innerHTML =
-        `Vos
-<img src="./img/${playerMove}-emoji.png" class="move-icon">
-<img src="./img/${computerMove}-emoji.png" class="move-icon">
+  //agarro el paragraph y muestro el resultado
+  document.querySelector(".js-result").innerHTML = resultado;
+
+  document.querySelector(".js-moves").innerHTML = `Vos
+<img src="./img/${playerMove.toLowerCase()}-emoji.png" class="move-icon">
+<img src="./img/${computerMove.toLowerCase()}-emoji.png" class="move-icon">
 Computadora`;
 
-    `Elegiste ${playerMove} - Computadora eligio ${computerMove}.`;
-
-}//nuevo al final del alert
+  `Elegiste ${playerMove} - Computadora eligio ${computerMove}.`;
+} //nuevo al final del alert

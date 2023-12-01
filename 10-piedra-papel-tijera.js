4fc3d772970c4400d57de4d561380eb76574f819
valrotkier <valrotkier@gmail.com>
2023-12-01 18:37:14 -0300

    probando un fix

--- 10-piedra-papel-tijera.js
+++ 10-piedra-papel-tijera.js
@@ -82,8 +82,8 @@ function playGame(playerMove) {
   document.querySelector(".js-result").innerHTML = resultado;
 
   document.querySelector(".js-moves").innerHTML = `Vos
-<img src="./img/${playerMove}-emoji.png" class="move-icon">
-<img src="./img/${computerMove}-emoji.png" class="move-icon">
+<img src="./img/${playerMove.toLowerCase()}-emoji.png" class="move-icon">
+<img src="./img/${computerMove.toLowerCase()}-emoji.png" class="move-icon">
 Computadora`;
 
   `Elegiste ${playerMove} - Computadora eligio ${computerMove}.`;

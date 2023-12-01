b8ed41a0305d1be5a0c63a92722a6cd79dd0f85f
valrotkier <valrotkier@gmail.com>
2023-12-01 18:13:59 -0300

    arreglando ruta js

--- 10-piedra-papel-tijera.js
+++ 10-piedra-papel-tijera.js
@@ -96,8 +96,8 @@ function playGame(playerMove) {
 
     document.querySelector('.js-moves').innerHTML =
         `Vos
-<img src="/img/${playerMove}-emoji.png" class="move-icon">
-<img src="/img/${computerMove}-emoji.png" class="move-icon">
+<img src="./img/${playerMove}-emoji.png" class="move-icon">
+<img src="./img/${computerMove}-emoji.png" class="move-icon">
 Computadora`;
 
     `Elegiste ${playerMove} - Computadora eligio ${computerMove}.`;
